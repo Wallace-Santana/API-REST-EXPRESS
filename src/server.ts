b7f422f23d9e73bd487fd8f28def77842b5bcecf
Wallace-Santana <wallace-santana@outlook.com.br>
2024-11-19 17:15:31 -0300

    instalacao express e server port

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log('Hello Word')
+import express from 'express';
+
+const PORT = 3333  
+
+const app = express();
+
+app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT: ${PORT}`));
